8e45e6bf5fe6bd288af7cfaa850e8579848a33a3
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-12-30 15:03:12 +0530

    Add files via upload

---
+++ PrintStatements.py
@@ -0,0 +1,16 @@
+a = 56
+b = 32
+print("Sum of a and b is: a+b",a+b,sep = "?")
+print(a,end = " ")
+print(b)
+#jkshduisegd
+"""Multi Line expression
+hjhcbdc"""
+print('''
+      hjbbydgfv
+      hjvgdbsjr
+      bjhsgduisehdejn
+      nujuhdiueskdc''')
+print("Sum of",a,"and",b,"is :",a+b)
+print("Sum of {} and {} is: {}".format(a,b,a+b))
+print(f"Sum of {a} and {b} is: {a+b}")
